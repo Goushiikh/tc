e2c42a018ea58160b7123c1e4e0a5b5ac252998d
Goushiikh <47105396+Goushiikh@users.noreply.github.com>
2019-02-18 16:26:39 +0530

    Create character.cpp

---
+++ character.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+char c;
+cout<<"enter the value";
+cin>>c;
+if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+{
+cout<<"yes";
+}
+else
+{
+cout<<"no";
+}
+getch();
+}
