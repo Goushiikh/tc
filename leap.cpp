c20ef961aefc5945ad8672fc3c65597a887a4444
Goushiikh <47105396+Goushiikh@users.noreply.github.com>
2019-02-20 15:21:46 +0530

    Create leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int year;
+clrscr();
+cout<<"enter the year";
+cin>>year;
+if(year%4==0)
+{
+cout<<"leap year";
+}
+else
+{
+cout<<"not a leap year";
+}
+getch();
+}
